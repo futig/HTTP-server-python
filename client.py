899ea50e2c115943508b583d44636ad66b45ba32
futig <everymusic8@gmail.com>
2024-08-18 20:49:12 +0500

    client tests

--- client.py
+++ client.py
@@ -2,7 +2,25 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('localhost', 5000))
-sock.sendall(bytes('Hello, world', encoding='UTF-8'))
-data = sock.recv(1024)
+
+# # test index page
+# with open("./tests/test_requests/index_request.txt", "r") as f:
+#     sock.sendall(f.read().encode("utf-8"))
+# data = sock.recv(2048)
+# print(data.decode("utf-8"))
+
+
+# # test login page
+# with open("./tests/test_requests/logger_name_request.txt", "r") as f:
+#     sock.sendall(f.read().encode("utf-8"))
+# data = sock.recv(2048)
+# print(data.decode("utf-8"))
+
+
+# test upload image
+with open("./tests/test_requests/upload_image_request.txt", "r") as f:
+    with open("./tests/test_media/LC-84.jpg", "rb") as image:
+        sock.sendall(f.read().encode("utf-8") + image.read())
+data = sock.recv(2048)
+print(data.decode("utf-8"))
 sock.close()
-print(data)
