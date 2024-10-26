58eb176370c36e52b99cd739e13109a3113f8293
futig <everymusic8@gmail.com>
2024-10-26 18:53:57 +0500

    tests

---
+++ run_tests.py
@@ -0,0 +1,11 @@
+import sys
+import unittest
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+loader = unittest.TestLoader()
+suite = loader.discover('Tests')
+
+runner = unittest.TextTestRunner()
+runner.run(suite)
